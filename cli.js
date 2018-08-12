19a83692a61e79de2968638525f1640363687a59
StepanyanS <infostepanyans@gmail.com>
2018-08-12 13:24:34 +0400

    Add main project

---
+++ cli.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const fs = require('fs');
+
+// import argv handlers
+const argvHandlers = require('./lib/argv-handlers');
+
+if(process.argv.slice(2)[0] === '-v') {
+    argvHandlers.versionhandler();
+}
+
+else if(process.argv.slice(2)[0] === 'generate' || process.argv.slice(2)[0] === 'g') {
+    argvHandlers.generateHandler(process.argv.slice(2)[1], process.argv.slice(2)[2]);
+}
+else if(process.argv.slice(2)[0] === 'help') {
+    argvHandlers.helpHandler();
+}
+else {
+    argvHandlers.errorHandler();
+}
---
+++ readme.md
@@ -0,0 +1,36 @@
+# Fdev CLI
+
+## Installation
+
+```bash
+npm install -g fdev-cli
+```
+
+## Version
+
+To check CLI version run:
+```bash
+fdev -v
+```
+
+## Usage
+
+```bash
+fdev help
+```
+
+### Generating Components, Pages
+
+You can use the `fdev generate` (or just `fdev g`) command to generate components/pages:
+```bash
+cd src
+fdev generate component COMPONENT-NAME
+fdev generate page PAGE-NAME
+```
+
+## Documentation
+
+The documentation for the Fdev CLI is located in this repo's [GitHub](https://github.com/stepanyans/fdev-cli).
+
+## License
+ISC
