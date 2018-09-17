c3448955ac9eb213b8b786f7ff76aca8a0b470b5
KarenNersisyan <karen@stdevmail.com>
2018-09-17 18:26:08 +0400

    add dynamic import

--- bin/bin.js
+++ bin/bin.js
@@ -1,24 +1,27 @@
 #!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var version_1 = require("./../commands/version");
-var help_1 = require("./../commands/help");
-var error_1 = require("../commands/error");
-var generate_1 = require("../commands/generate");
-var new_1 = require("./../commands/new");
 var _a = process.argv, args = _a.slice(2);
 if (args[0] === 'version' || args[0] === '-v') {
-    version_1.versionHandler.handle();
+    Promise.resolve().then(function () { return require('../commands/version'); }).then(function (m) {
+        m.versionHandler.handle();
+    });
 }
 else if (args[0] === 'new') {
-    new_1.newHandler.handle(args[1], args[2]);
+    Promise.resolve().then(function () { return require('../commands/new'); }).then(function (m) {
+        m.newHandler.handle(args[1], args[2]);
+    });
 }
 else if (args[0] === 'generate' || args[0] === 'g') {
-    generate_1.generateHandler.handle(args[1], args[2]);
+    Promise.resolve().then(function () { return require('../commands/generate'); }).then(function (m) {
+        m.generateHandler.handle(args[1], args[2]);
+    });
 }
 else if (args[0] === 'help' || args[0] === '-h') {
-    help_1.helpHandler.handle();
+    Promise.resolve().then(function () { return require('../commands/help'); }).then(function (m) {
+        m.helpHandler.handle();
+    });
 }
 else {
-    error_1.errorHandler.handle(args[0]);
+    Promise.resolve().then(function () { return require('../commands/error'); }).then(function (m) {
+        m.errorHandler.handle(args[0]);
+    });
 }
--- bin/bin.ts
+++ bin/bin.ts
@@ -1,25 +1,29 @@
 #!/usr/bin/env node
 
-import { versionHandler } from './../commands/version';
-import { helpHandler } from './../commands/help';
-import { errorHandler } from '../commands/error';
-import { generateHandler } from '../commands/generate';
-import { newHandler } from './../commands/new';
-
 const [, , ...args]: string[] = process.argv;
 
 if(args[0] === 'version' || args[0] === '-v') {
-    versionHandler.handle();
+    import('../commands/version').then(m => {
+        m.versionHandler.handle();
+    });
 }
 else if(args[0] === 'new') {
-    newHandler.handle(args[1], args[2]);
+    import('../commands/new').then(m => {
+        m.newHandler.handle(args[1], args[2]);
+    })
 }
 else if(args[0] === 'generate' || args[0] === 'g') {
-    generateHandler.handle(args[1], args[2]);
+    import('../commands/generate').then(m => {
+        m.generateHandler.handle(args[1], args[2]);
+    });
 }
 else if(args[0] === 'help' || args[0] === '-h') {
-    helpHandler.handle();
+    import('../commands/help').then(m => {
+        m.helpHandler.handle();
+    });
 }
 else {
-    errorHandler.handle(args[0]);
+    import('../commands/error').then(m => {
+        m.errorHandler.handle(args[0]);
+    });
 }
