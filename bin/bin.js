4652f4d4424882b039254ff7bd990ff8dfb5970b
StepanyanS <infostepanyans@gmail.com>
2018-09-22 21:49:32 +0400

    compile fixes

--- bin/bin.js
+++ bin/bin.js
@@ -9,16 +9,19 @@ var new_handler_1 = require("./../commands/new.handler");
 var undefined_handler_1 = require("./../commands/undefined.handler");
 var _a = process.argv, args = _a.slice(2);
 switch (args[0]) {
-    case 'version' || '-v':
+    case 'version':
+    case '-v':
         version_handler_1.versionHandler.handle();
         break;
     case 'new':
         new_handler_1.newHandler.handle(args[1], args[2]);
         break;
-    case 'generate' || 'g':
+    case 'generate':
+    case 'g':
         generate_handler_1.generateHandler.handle(args[1], args[2]);
         break;
-    case 'help' || '-h':
+    case 'help':
+    case '-h':
         help_handler_1.helpHandler.handle();
         break;
     case undefined:
